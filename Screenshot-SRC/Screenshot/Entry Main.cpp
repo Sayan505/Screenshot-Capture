0b5861e9852c0d3abf4b008a83804f68f1097345
Sayan Bhattacharya <bsayan12345@gmail.com>
2020-05-18 19:10:47 +0530

    Improve performance

--- Screenshot-SRC/Screenshot/Entry Main.cpp
+++ Screenshot-SRC/Screenshot/Entry Main.cpp
@@ -4,74 +4,90 @@
 #include <iostream>
 #include <Windows.h>
 #include <string>
-#include <ctime>
-#include <tchar.h> //_t MACRO
+#include <chrono>
 
-#include <Gdiplus.h> ////USING GDI+
+#include <gdiplus.h>//USING GDI+
 #include <atlimage.h> //USING ACTIVE TEMPLATE LIBRARY
 
 
 #define KEY1 1
 
-using namespace std;
+using namespace std::chrono;
 
-//ENTRY POINT
-int __fastcall _tmain(int argc, TCHAR *argv[]) {
-	int x;
-	
-	if (RegisterHotKey(NULL, KEY1, MOD_CONTROL | MOD_NOREPEAT, 0x7B)) {	//0x7B = F12.
-		_tprintf(_T("GLOBAL HOTKEY CTRL+F12 IS REGISTERED ON MEMORY \n \n"));
-	}
 
+int main(int argc, char **argv) {
+	__int64 t;
+
+	//for geting screen input and screen reso
 	MSG msg;
-	DEVMODE dm;
+	DEVMODEA dm;
+
+	//create device context for screen and memory
+	HDC hScreenDC = NULL, hMemoryDC = NULL;
+
+	//handle to Bitmaps
+	HBITMAP hBitmap = NULL, hOldBitmap = NULL;
+
+	CString _filename;
+
+	CImage img;
+
+
 	dm.dmSize = sizeof(DEVMODE);
 
+	EnumDisplaySettingsA(0, ENUM_CURRENT_SETTINGS, &dm);
+
+	//set reso
+	int width = dm.dmPelsWidth;
+	int height = dm.dmPelsHeight;
+
+	t = duration_cast<std::chrono::milliseconds>
+		(system_clock::now().time_since_epoch()).count();
+	
+	std::ios_base::sync_with_stdio(false);
+
+	if (RegisterHotKey(NULL, KEY1, MOD_CONTROL | MOD_NOREPEAT, 0x7B)) {	//0x7B = F12.
+		std::cout << "GLOBAL HOTKEY CTRL+F12 IS REGISTERED ON MEMORY\n\n";
+	}
+
+
 	while (GetMessage(&msg, NULL, 0, 0) != 0) {
-		Sleep(125);
+		Sleep(10);
 	
 		if (msg.message == WM_HOTKEY)
 		{
-				_tprintf(_T("HOTKEY RECEIVED \n"));
-				//MAKE DEVICE CONTEXT FOR THE SCREEN.
-				HDC hScreenDC = CreateDC("DISPLAY", NULL, NULL, NULL);
-				//CREATE A DEVICE CONTEXT FOR MEMORY.
-				HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
-				
-				EnumDisplaySettingsA(0, ENUM_CURRENT_SETTINGS, &dm); // <3
-				int width = dm.dmPelsWidth;
-				int height = dm.dmPelsHeight;
+			//populate DCs
+			hScreenDC = CreateDC("DISPLAY", NULL, NULL, NULL);
+			hMemoryDC = CreateCompatibleDC(hScreenDC);
 
-				HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
-				//NEW BITMAP.	
-				HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemoryDC, hBitmap);
-				BitBlt(hMemoryDC, 0, 0, width, height, hScreenDC, 0, 0, SRCCOPY);
-				
-				//FINAL BITMAP.
-				
-				hBitmap = (HBITMAP)SelectObject(hMemoryDC, hOldBitmap);
-				
-				//COPY hBitmap TO Clipboard.
-				OpenClipboard(NULL); //LOCK CLIPBOARD.
-				EmptyClipboard(); //CLEAR CLIPBOARD.
-				SetClipboardData(CF_BITMAP, hBitmap);
-				CloseClipboard(); //RELEASE CLIPBOARD.
+			hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
 
-				// CLEAR DEVICE CONTEXT/MEMORY. NO NEED TO ReleaseDC().
-				DeleteDC(hMemoryDC);
-				DeleteDC(hScreenDC);
-				
-				//IMAGE SAVING CImage
-				srand(time(NULL)); //GENERATE RANDOM NUMBER.
-				x = rand(); //STORE RANDOM NUMBER.
-				CString s;
-				s.Format(_T("%d"), x); //CONVERT INT TO LPCTSTR
-				CImage img;
-				img.Attach(hBitmap);
-				img.Save(s+".BMP", Gdiplus::ImageFormatBMP); //SAVE TO FILE.
-				
-				_tprintf(_T("BITMAP COPIED TO CLIPBOARD, SAVED AND CLEARED DEVICE CONTEXT\n \n"));
+			//store result
+			hOldBitmap = (HBITMAP) SelectObject(hMemoryDC, hBitmap);
+
+			//copy color data from hScreenDC to hMemoryDC
+			BitBlt(hMemoryDC, 0, 0, width, height, hScreenDC, 0, 0, SRCCOPY);
+
+			//FINAL BITMAP
+			hBitmap = (HBITMAP) SelectObject(hMemoryDC, hOldBitmap);
+
+
+			//SAVING CImage
+			++t;
+
+			_filename.Format("%llu", t); //convert __int64 to LPCTSTR
+
+			img.Attach(hBitmap);
+			img.Save(_filename + ".BMP", Gdiplus::ImageFormatBMP); //save
+
+			std::cout << "Screenshot Captured!\n\n";
+
+			img.Destroy();
+
+			ResetDCA(hMemoryDC, &dm);
+			ResetDCA(hScreenDC, &dm);
 		}
 	}
+
 	return 0;
 }
